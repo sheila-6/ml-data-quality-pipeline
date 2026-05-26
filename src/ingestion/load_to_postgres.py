7ed3c1c628cb6a1b05ffd656c0da9e0f8a30710c
sheila-6 <167766351+sheila-6@users.noreply.github.com>
2026-05-26 20:44:25 +0200

    Change DB_PASSWORD to a more secure value
    Updated database password for security.

--- src/ingestion/load_to_postgres.py
+++ src/ingestion/load_to_postgres.py
@@ -6,11 +6,11 @@ import os
 # 1. Database Configuration
 # ---------------------------------------------------
 
-DB_USER = "postgres"              # ← change if needed
-DB_PASSWORD = "Kiptoo.4418"     # ← change this
+DB_USER = "postgres"              #  change if needed
+DB_PASSWORD = "xx"     # change this
 DB_HOST = "localhost"
 DB_PORT = "5432"
-DB_NAME = "postgres"    # ← change this
+DB_NAME = "postgres"    # change this
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL)
